d6fa23f23e6c072a400bd19dfcaf81edae520e6a
ChivalrousDig <ChivalrousDig@gmail.com>
2013-02-19 14:05:09 +0800

    确定过滤交互的规范

--- src/main/java/org/gitchina/ks/web/KeywordsSpammingServer.java
+++ src/main/java/org/gitchina/ks/web/KeywordsSpammingServer.java
@@ -1,9 +1,33 @@
 package org.gitchina.ks.web;
 
+import org.gitchina.wordfiter.result.FilteredResult;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * 
+ * @ClassName:     KeywordsSpammingServer
+ * @Description:TODO(提供上层服务调用进行关键字过滤)
+ * @author:    Chivalrous
+ * @date:        2013-2-19 下午2:01:11
+ */
 @Controller
 public class KeywordsSpammingServer {
-	
-	
+	/**
+	 * 
+	 * @Title: filteringMaliciousKeyWords
+	 * @Description: TODO(过滤方法)
+	 * @param: @param KeyWords 需要过滤的字符串
+	 * @param: @return  返回已过滤的数据的相关参数
+	 * @return: FilteredResult   
+	 * @throws
+	 */
+	@RequestMapping(value = "/filteringMaliciousKeyWords", method = RequestMethod.POST)
+	public @ResponseBody FilteredResult filteringMaliciousKeyWords(@RequestBody String KeyWords  ) {
+		
+		return null;
+	}
 }
